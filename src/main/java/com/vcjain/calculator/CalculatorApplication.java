8443f7d09305b0b50a96c5c9dd71b588f2f29623
vcjain <vikashkumarjain@gmail.com>
2023-12-14 21:06:48 +0530

    Update CalculatorApplication.java

--- src/main/java/com/vcjain/calculator/CalculatorApplication.java
+++ src/main/java/com/vcjain/calculator/CalculatorApplication.java
@@ -9,7 +9,10 @@ public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 
-		
+		try{
+			int i =0;
+		}catch(Exception e){
+		}
 	}
 
 }
